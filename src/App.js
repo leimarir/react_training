cbf8b97d4dd1b70eb6f71d1ce8f3308484b15492
leimarir <leimar.i.ros@gmail.com>
2023-02-07 06:26:26 +0800

    Added arrays, map, button, and change state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+class App extends Component {
+  //Task 2: Using array and map function
+  fruits = ["apple", "banana", "mango", "coconut", "orange"];
+  listFruits = this.fruits.map((fruits) =>
+    <li>{fruits}</li>
   );
+
+  state = {
+    text : 'Hello there!'
+  }
+
+  //Task 3: Using button to change state
+  handleClick = () =>{
+    this.setState(
+      {
+        text : 'Goodbye'
+      }
+    )
+  }
+
+  render(){
+    return(
+      <div>
+        <ul>{this.listFruits}</ul>
+        <h2>Message</h2>
+          <p>{this.state.text}</p>
+        <button onClick={this.handleClick}>
+          Change State
+        </button>
+      </div>
+    );
+  }
+
 }
 
 export default App;
+
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;

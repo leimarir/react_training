3f45dfc941952686642dd4f594eb969163cc8b84
leimarir <leimar.i.ros@gmail.com>
2023-02-08 07:17:32 +0800

    Added data and modals

---
+++ src/data.js
@@ -0,0 +1,33 @@
+const data = {
+    "Employees" : [
+        {
+        "name":"Romin Irani",
+        "age":"27",
+        "DOB":"01/01/96",
+        "salary":"30000",
+        "designation":"Developer",
+        "image":"../../image/web_developer1.png",
+        "department":"Cloud"
+        },
+        {
+        "name":"Neil Irani",
+        "age":"24",
+        "DOB":"01/05/99",
+        "salary":"32000",
+        "designation":"Developer",
+        "image":"../../image/web_developer2.png",
+        "department":"Cloud"
+        },
+        {
+        "name":"Tom Hanks",
+        "age":"28",
+        "DOB":"01/30/95",
+        "salary":"29000",
+        "designation":"Recruiter",
+        "image":"../../image/web_developer3.png",
+        "department":"HR"
+        }
+    ]
+};
+
+export default data;

12d5202703a97eb4a6bd8327689f935ada630e10
leimarir <leimar.i.ros@gmail.com>
2023-02-08 09:23:18 +0800

    Added passing of data from 1 component to another

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
 import Modal from './Modal';
+import Child from './Child';
 
 class App extends Component {
+    state = {
+        grocery : ['salt', 'soy sauce', 'oil', 'rice', 'vinegar']
+    };
+
     render() {
         return (
             <div>
                 <Modal />
+                <div>
+                    <Child data = {this.state} />
+                </div>
             </div>
         );
     }
